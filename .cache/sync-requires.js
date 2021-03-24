c6ff3ff09e81c335009b1c8649ba323416dc5082
CaioF <caio.fleury.r@gmail.com>
2021-03-24 15:42:59 -0300

    Minor fixes

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/home/kraft/Work/Projects/websites/CaioF.github.io/.cache/caches/gatsby-plugin-offline/app-shell.js"))),
+  "component---node-modules-lekoarts-gatsby-theme-cara-src-templates-cara-tsx": hot(preferDefault(require("/home/kraft/Work/Projects/websites/CaioF.github.io/node_modules/@lekoarts/gatsby-theme-cara/src/templates/cara.tsx")))
+}
+
